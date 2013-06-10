a103049c517bc7b685854f2bdc2d6cce663f95a3
Louis <louis.amore.iut2@gmail.com>
2013-06-10 15:59:39 +0200

    Modifications on the partial voletgauche.html.twig

--- src/Innova/ActivityBundle/Resources/public/js/accordeon.js
+++ src/Innova/ActivityBundle/Resources/public/js/accordeon.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	 		$(this).removeClass("icon-folder-open");
 	 	}
 	 	else if ($(this).parent().is("div")) {
-	 		$(this).children("img").attr("src","img/folder-close.png");
+	 		$(this).children("img").attr("src","{{ asset('bundles/innovaactivity/img/folder-close.png') }}");
 	 	}
    
 		//IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
@@ -38,7 +38,7 @@ $(document).ready(function() {
 		 		$(this).removeClass("icon-folder-close");
 	 		}
 	 		else if ($(this).parent().is("div")) {
-	 			$(this).children("img").attr("src","img/folder-open.png");
+	 			$(this).children("img").attr("src","{{ asset('bundles/innovaactivity/img/folder-open.png') }}");
 	 		}
 			  
 			//OPEN THE SLIDE
