d90cb42ca7708eb969023b817c50b3c81f117d8b
Louis <louis.amore.iut2@gmail.com>
2013-06-10 14:46:41 +0200

    Migrating things to Symfony

---
+++ src/Innova/ActivityBundle/Resources/views/Activity/partials/barreoutil.inc.php
@@ -0,0 +1,9 @@
+		
+		
+<div class="row-fluid boutMenu">
+	
+		<button type="submit" class="btn outil liaison pull-right menuTools" data-toggle="modal" data-target="#liaisonModal">
+			<i class="icon-wrench "></i>Livrets de liaison
+		</button>	
+	
+</div>
---
+++ src/Innova/ActivityBundle/Resources/views/Activity/partials/corps.inc.php
@@ -0,0 +1,18 @@
+	<!-- Fenêtres modales -->
+	<?php require_once("modals.inc.php"); ?>
+	<!-- Chemins -->
+	<?php require_once("volethaut.inc.php"); ?>
+	<!-- Volet édition arborescence -->
+	<?php require_once("volethaut2.inc.php"); ?> 
+<!-- Corps -->
+<div class=" corps ">
+			
+<!-- Titre espace -->
+	<?php 	require_once("week1.inc.php"); 
+			require_once("week2.inc.php"); 
+			require_once("week3.inc.php"); 
+			require_once("week4.inc.php"); 
+			require_once("week5.inc.php"); 
+			require_once("etape2.inc.php"); 
+			require_once("parcourspass.inc.php"); ?>
+</div>
