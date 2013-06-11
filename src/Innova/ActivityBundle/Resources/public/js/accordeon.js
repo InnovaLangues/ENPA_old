49a894556fcf2e4a8f45e4680d345e1c44c77290
Louis <louis.amore.iut2@gmail.com>
2013-06-11 10:00:31 +0200

    Adding images in voletgauche.html.twig

--- src/Innova/ActivityBundle/Resources/public/js/accordeon.js
+++ src/Innova/ActivityBundle/Resources/public/js/accordeon.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
 	 	}
 	 	else if ($(this).parent().is("div")) {
 	 		$(this).children("img").attr("src","{{ asset('bundles/innovaactivity/img/folder-close.png') }}");
+	 		
 	 	}
    
 		//IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
