1cd889a9d39dad305c332f359f8f6ffb2fde6d36
Donovan Tengblad <purplefish32@gmail.com>
2013-06-27 11:34:02 +0200

    Fixed cp

--- src/Innova/AppBundle/Command/LoadDevCommand.php
+++ src/Innova/AppBundle/Command/LoadDevCommand.php
@@ -6,7 +6,7 @@ namespace Innova\AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;http://www.siteduzero.com/informatique/tutoriels/developpez-votre-site-web-avec-le-framework-symfony2/les-relations-bidirectionnelles
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\HttpKernel;
