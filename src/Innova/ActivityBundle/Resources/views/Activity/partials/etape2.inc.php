c44b4c2584b42d4dd4f3a1a2073dfb26d9945c2e
Donovan Tengblad <purplefish32@gmail.com>
2013-07-01 09:57:36 +0200

    Fixed broken HTML

--- src/Innova/ActivityBundle/Resources/views/Activity/partials/etape2.inc.php
+++ src/Innova/ActivityBundle/Resources/views/Activity/partials/etape2.inc.php
@@ -10,47 +10,43 @@
 				
 		</div>
 	</div>
+    <div class="row-fluid">
+        <div class="span12  tabbable tabs-left contenu">
+            <ul class="nav nav-tabs">
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active tabDescr">
+                    <h3>Etape 2 – Things around you</h3>
+                    <img src="img/util/parcourspass/etape2/entete.jpg" class="imgCorps"/>
+                    <p>
+                        Cette étape 2 est divisée en 5 semaines :
+                    </p>
+                    <ul>
+                        <li>Week 1- Describing people</li>
+                        <li>Week 2- Describing places</li>
+                        <li>Week 3- Describing things</li>
+                        <li>Week 4- Let's Review!</li>
+                        <li>Week 5- Writing</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
-		<div class="row-fluid">
-		<div class="span12  tabbable tabs-left contenu">
-		<ul class="nav nav-tabs">
-			
-		</ul>
-		 
-			<div class="tab-content">
-				<div class="tab-pane active tabDescr">
-					<h3>Etape 2 – Things around you</h3>
-					<img src="img/util/parcourspass/etape2/entete.jpg" class="imgCorps"/>
-					<p>
-						Cette étape 2 est divisée en 5 semaines : 
-						<ul>
-							<li>Week 1- Describing people</li>
-							<li>Week 2- Describing places</li>
-							<li>Week 3- Describing things</li>
-							<li>Week 4- Let's Review!</li>
-							<li>Week 5- Writing</li>
-						</ul>
-					</p>
+    <!-- <div class="row-fluid">
+    <div class="span12">
+        <h3>Etape 2 – Things around you</h3>
 
-				</div>
+        <p>Ce niveau correspond à celui de l’unité dans l’arborescence d’Esprit.
+            Le niveau en question contient ici plusieurs « espace activités »
+            (ex groupe d’actions dans Esprit) dont un seul est actuellement présenté dans
+            ce prototype.</p>
 
-		</div>
-	</div>
-</div>
-		<!-- <div class="row-fluid">
-		<div class="span12">
-			<h3>Etape 2 – Things around you</h3>
-
-			<p>Ce niveau correspond à celui de l’unité dans l’arborescence d’Esprit. 
-				Le niveau en question contient ici plusieurs « espace activités » 
-				(ex groupe d’actions dans Esprit) dont un seul est actuellement présenté dans 
-				ce prototype.</p>
-
-			<p>Comme l’illustre ce texte, ce niveau, comme tout niveau quel qu’il soit peut 
-				(mais ne doit pas obligatoirement) être accompagné d’une description généré 
-				à partie d’un éditeur enrichi (avec toutes les possibilités que l’éditeur offre 
-				(lien internet, voir mise à disposition de fichier pour affichage ou téléchargement).</p>
-		</div>
-	</div> -->
+        <p>Comme l’illustre ce texte, ce niveau, comme tout niveau quel qu’il soit peut
+            (mais ne doit pas obligatoirement) être accompagné d’une description généré
+            à partie d’un éditeur enrichi (avec toutes les possibilités que l’éditeur offre
+            (lien internet, voir mise à disposition de fichier pour affichage ou téléchargement).</p>
+    </div>
+</div> -->
 	
 </div>
